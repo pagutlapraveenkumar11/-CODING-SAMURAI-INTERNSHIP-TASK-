902c1d5314ccc9c86d8f07ca6163cb83723fbade
pagutlapraveenkumar11 <praveenpagutla08@gmail.com>
2025-03-01 12:20:15 +0530

    Add files via upload

---
+++ Sample calculator project.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+num1=int(input("Give 1st number:"))
+num2=int(input("Give 2st number:"))
+Operator = input("Give operator:")
+if Operator == "+":
+   print(f"Addition of 2 numbers is {num1+num2}")
+elif Operator == "-":
+   print(f"Subtraction of 2 numbers is {num1-num2}")
+elif Operator == "*":
+   print(f"Multiplication of 2 numbers is {num1*num2}")
+else:
+   print(f"Division of 2 numbers is {num1/num2}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
